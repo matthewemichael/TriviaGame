e9f0f9f9d1263628199df1499ad372d7b89630f4
Matt Michael <matthewemichael@gmail.com>
2019-08-16 16:28:13 -0500

    game now displays possible answers. timer counts down from 30 and stops at 0.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,19 +3,19 @@ $( document ).ready(function() {
 const quizQuestions = [
     {
         question: "What is the capitol of Tennessee?",
-        choices: ["Nashville", "Knoxville", "Memphis"],
+        answers: ["Nashville", "Knoxville", "Memphis"],
         correctAnswer: "Nashville"
     },
 
     {
         question: "How many congressmen does TN have in the House of Representatives?",
-        choices: ["6", "7", "8", "9"],
+        answers: ["6", "7", "8", "9"],
         correctAnswer: "9" 
     },
     
     {
         question: "How many presidents have been from Tennessee?",
-        choices: ["1", "2", "3", "4"],
+        answers: ["1", "2", "3", "4"],
         correctAnswer: "3" 
     }
 ];
@@ -29,14 +29,49 @@ let losses = 0;
 let unanswered = 0;
 let timer;
 
-// Display the question and choices in the browser
+// Start a 30 second timer for each question
 
-function loadQuestion() {
-    const question = quizQuestions[currentQuestion].question; 
-    const choices = quizQuestions[currentQuestion].choices; 
-
-    $('#game').html('<h4>' + question + '<h4>');
+function timeUp() {
+    clearInterval(timer);
 }
+
+function countDown() {
+    counter--;
+
+    $('#time').html(`
+        Timer: ${counter}<p>
+    `)
+    if (counter === 0) {
+        timeUp();
+    }
+}
+
+// Display the question and answers in the browser
+function loadQuestion() {
+    counter = 30;
+    timer = setInterval(countDown, 1000);
+
+    const question = quizQuestions[currentQuestion].question; 
+    const answers = quizQuestions[currentQuestion].answers; 
+
+    $('#time').html(`
+        Timer: ${counter}<p>
+    `)    
+    $('#game').html(`
+        <h4>${question}</h4><br>
+        ${loadAnswers(answers)}
+    `);
+}
+
+function loadAnswers(answers){
+    let result = '';
+    for (let i = 0; i < answers.length; i++) {
+        result += `<p class="choice" data-answer="${answers[i]}">${answers[i]}</p>`
+    }
+
+    return result;
+};
+
 
 $( ".start" ).click(function()  {
     $("#game-start").remove();
